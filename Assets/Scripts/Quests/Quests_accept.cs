31bb2643d166b26c89b08ad4d07e560378467d2b
lostdata2 <sharipov.vasiliy0202@gmail.com>
2025-03-14 12:21:47 +0300

    fix for buttons

--- Assets/Scripts/Quests/Quests_accept.cs
+++ Assets/Scripts/Quests/Quests_accept.cs
@@ -6,6 +6,7 @@ public class Quests_accept : MonoBehaviour
 {
     [SerializeField] private ShipInput input;
     [SerializeField] private GameObject dialog;
+    [SerializeField] private GameObject buttons;
     [SerializeField] private dialog M_dialog;
     [SerializeField] private GameObject accept_notify;
     void OnCollisionStay(Collision collision)
@@ -16,6 +17,7 @@ public class Quests_accept : MonoBehaviour
             if (input.GetInteractButton()) {
                 accept_notify.SetActive(false);
                 dialog.SetActive(true);
+                buttons.SetActive(false);
                 M_dialog.say();
                 Destroy(this.gameObject);
             }
